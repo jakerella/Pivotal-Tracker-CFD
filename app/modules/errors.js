533b3292e6a8d05328562788bb6ca1f675cdb7f8
jakerella <jordan@jordankasper.com>
2013-05-17 15:49:40 -0500

    Added DatabaseError to app-wide error types

--- app/modules/errors.js
+++ app/modules/errors.js
@@ -10,6 +10,16 @@ util.inherits(HttpError, Error);
 HttpError.prototype.name = 'HTTP Error';
 HttpError.prototype.status = 500;
 
+var DatabaseError = function (msg, status, constr) {
+    Error.captureStackTrace(this, constr || this);
+    this.message = msg || 'HTTP Error';
+    this.status = status || 500;
+};
+util.inherits(DatabaseError, Error);
+DatabaseError.prototype.name = 'Database Error';
+DatabaseError.prototype.status = 500;
+
 module.exports = {
-  HttpError: HttpError
+  HttpError: HttpError,
+  DatabaseError: DatabaseError
 };
